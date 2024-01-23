6a4e57c3b36354871724f728323551775d220617
Valentin Launay <otctrojan@gmail.com>
2024-01-23 16:01:10 +0100

    final deploy + sass

--- src/components/HousingDisplay/HousingDisplay.js
+++
@@ -1,8 +0,0 @@
-import { useParams, Navigate } from "react-router-dom";
-import Carrousel from "..//Carrousel/Carrousel";
-import Collapse from "../Collapse/Collapse";
-import Data from "../../datas/logements.json";
-import Tags from "../Tag/Tags";
-import Host from "../Host/Host";
-
-const 
