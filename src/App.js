c4f7ea1212efbb45e2ee95de8f7fd0f5edda20ac
Valentin Launay <otctrojan@gmail.com>
2024-01-23 11:50:17 +0100

    deploy fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Routes basename="/P6OC">
+        <Routes basename="/P6OC/">
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/logement/:id" element={<Housing />} />
